9a9fb06742d1efa3847f642abd81dc88c228c25b
DhineshCool <dhineshcool585@gmail.com>
2026-03-06 20:10:11 +0530

    sm6150-common : remove lineage stuffs

--- common.mk
+++ common.mk
@@ -27,10 +27,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-custom
 
 # A/B
 AB_OTA_UPDATER := true
